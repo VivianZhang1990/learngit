caca2835d8e63aa27b672fd0d3b39a71476d52d7
vivian <vivian_zhang@ymtc.com>
2018-04-04 15:06:16 +0800

    modify test.py

--- test.py
+++ test.py
@@ -6,8 +6,5 @@ import os
 import subprocess
 
 sys.path.append('/home/fw_test/test_framework')
-cmd='cd /home/fw_test/test_framework \npython run_tool.py'
-
-# os.popen('/home/vivian/20180326/test_scripts/mytest1.py')
-# cmd='python /home/vivian/20180326/test_framework/run_tool.py'
+cmd='print test.py'
 print cmd
