2550f47f270700d3267ed281990f653860acb29c
vivian <vivian_zhang@ymtc.com>
2018-04-04 14:57:50 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import subprocess
+
+sys.path.append('/home/fw_test/test_framework')
+cmd='cd /home/fw_test/test_framework \npython run_tool.py'
+
+# os.popen('/home/vivian/20180326/test_scripts/mytest1.py')
+# cmd='python /home/vivian/20180326/test_framework/run_tool.py'
+print cmd
